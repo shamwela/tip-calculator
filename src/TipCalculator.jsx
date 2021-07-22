e6adb62311da58ad7e04ff59407e6c897e86ac05
Sha Mwe La <shamwela2002@gmail.com>
2021-07-22 14:33:55 +0630

    Add more styles

---
+++ src/TipCalculator.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './TipCalculator.sass'
+
+export default class TipCalculator extends Component {
+  render() {
+    return (
+      <main>
+        <section id='tip-calculator'></section>
+      </main>
+    )
+  }
+}
