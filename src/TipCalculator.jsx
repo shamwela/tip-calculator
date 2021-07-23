871d1ae371d6007618f73329bfa32c20803f2546
Sha Mwe La <shamwela2002@gmail.com>
2021-07-23 12:23:38 +0630

    Create a rough layout

--- src/TipCalculator.jsx
+++ src/TipCalculator.jsx
@@ -5,7 +5,40 @@ export default class TipCalculator extends Component {
   render() {
     return (
       <main>
-        <section id='tip-calculator'></section>
+        <img id='logo' src='./logo.svg' alt='Logo' />
+        <article id='tip-calculator'>
+          <section id='input-section'>
+            <section id='bill'>
+              <label htmlFor=''>Bill</label>
+              <input type='number' />
+            </section>
+            <section id='select-tip'>
+              <label htmlFor=''>Select Tip %</label>
+              {/* 6 buttons will be here */}
+            </section>
+            <section id='people'>
+              <label htmlFor=''>Number of People</label>
+              <input type='number' />
+            </section>
+          </section>
+          <section id='output-section'>
+            <section>
+              <div>
+                Tip Amount
+                <br />/ person
+              </div>
+              <div>$4.27</div>
+            </section>
+            <section>
+              <div>
+                Total
+                <br />/ person
+              </div>
+              <div>$4.27</div>
+            </section>
+            <button>RESET</button>
+          </section>
+        </article>
       </main>
     )
   }
